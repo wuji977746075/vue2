a33217aec7fbdc81a00df7ed9faa32016a856993
wuji977746075 <wuji977746075@github.com>
2017-03-09 11:39:18 +0800

    litter change

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,7 +1,8 @@
 <template>
 <div>
-  <h1 class="logo" v-text="title"></h1>
-  <h1 class="logos" v-text="tit"></h1>
+  <h1 class="logo" v-text="headerTitle"></h1>
+  <h1 v-text="menus"></h1>
+  <div v-text="news"></div>
   <router-link to="/welcome">welcome.html</router-link>
   <router-link to="/content">content.html</router-link>
   <ul class="list">
@@ -10,49 +11,50 @@
 </div>
 </template>
 
-
 <script>
+import { mapState } from 'vuex'
 // import {savaToLocal} from '../common/js/store';
 // import {loadFromlLocal} from '../common/js/store';
 export default {
   data() { //组件数据
     return {
       list:[],
-      title: 'cnsodejs Api Test',
-      tit:'state check',
+      headerTitle: '',
+      news:'',
+      menus:'',
     }
   },
   created () { // 组件创建完
     // this.get_data()
   },
   mounted() { // 组件加载完
-    setTimeout(() => {
-    //   //localstorge
+    // setTimeout(() => {
+    //localstorge
     //   savaToLocal('index', 'title', 'title')
     //   this.title = loadFromlLocal('index', 'title', '未找到title')
     //store
-    this.tit = this.$store.state.headerTitle
-    this.$store.state.headerTitle = this.title
-    }, 2000)
+    // }, 2000)
+    // this.$store.state.headerTitle = this.headerTitle
+    // this.news = this.$store.state.menus
   },
   computed: { // 实时计算
-    // ...mapState([
-    //   'headerTitle', 'menuShow'
-    // ])
+    ...mapState([ //对象展开运算符,store与局部计算属性混合
+      'news','headerTitle','menus'
+    ])
     // getTitle () {
     //   return store.state.headerTitle
     // }
   },
   methods: { // 组件方法
-    get_data: function(params) {
-      var v = this
-      if (!params) params = {}
+    // get_data: function(params) {
+    //   var v = this
+    //   if (!params) params = {}
       // 我们这里用全局绑定的 $api 方法来获取数据，方便吧~
       // v.$api.get('topics', params, function(r) {
       //   console.log(r)
       //   v.list = r.data
       // })
-    },
+    // },
   },
 }
 </script>
